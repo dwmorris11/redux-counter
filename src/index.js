2ab9f95258d98aa61d2f1b819fe832fc7e8ca39a
Dustin Morris <dustinnuc@gmail.com>
2020-09-19 08:04:16 -0700

    updated react-scripts, added increment and decrement counters

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
+import './styles.scss';
 
 const initialState = {
-  count = 0;
+  count: 0,
 }
 
 //This is changed to a constant to allow
 //debugger to work in case of misspelling automatically
 const INCREMENT = 'INCREMENT';
 
+const DECREMENT = 'DECREMENT';
+
+//action creator
 const incrementValue = () => ({
   type: INCREMENT,
+});
+
+const decrementValue = () => ({
+  type: DECREMENT,
 });
 
 const reducer = (state = initialState, action) => {
@@ -22,19 +29,27 @@ const reducer = (state = initialState, action) => {
       count: state.count + 1,
     };
   }
+  if (action.type === DECREMENT){
+    return {
+      count: state.count - 1,
+    };
+  }
+  //always return some state if everything falls through
   return state;
 }
 
-import './styles.scss';
+const store =  createStore(reducer);
 
 class Counter extends Component {
+
   render() {
+    const { count, increment, decrement } = this.props;
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
           <button>Reset</button>
         </section>
       </main>
@@ -42,14 +57,24 @@ class Counter extends Component {
   }
 }
 
-const mapStateToProps = () => {};
-const mapDispatchToProps = () => {};
+// takes the entire state in the component
+// but takes off the things you want off the state tree
+// in this case returning the entire state of the world
+const mapStateToProps = (state) => { return state;};
 
-connect()(Counter);
+// takes the dispatch argument pointing to store.dispatch
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment() { dispatch(incrementValue()) },
+    decrement() { dispatch(decrementValue())}
+  };
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 render(
   <Provider store={store}>
-    <Counter />
+    <CounterContainer />
   </Provider>,
   document.getElementById('root')
 );
