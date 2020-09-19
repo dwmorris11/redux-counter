73103715f4c8b9367b4ec1092a16d6fee0258f8f
Dustin Morris <dustinnuc@gmail.com>
2020-09-18 19:03:29 -0700

    Basic redux store added to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+const initialState = {
+  count = 0;
+}
+
+//This is changed to a constant to allow
+//debugger to work in case of misspelling automatically
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT){
+    return {
+      count: state.count + 1,
+    };
+  }
+  return state;
+}
+
 import './styles.scss';
 
 class Counter extends Component {
@@ -18,4 +42,15 @@ class Counter extends Component {
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = () => {};
+const mapDispatchToProps = () => {};
+
+connect()(Counter);
+
+render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root')
+);
+
